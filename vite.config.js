080d087df191676367ece78a94a040405fc0f99b
Chris Leary <csleary@gmail.com>
2025-08-09 13:45:24 +0200

    Use image defaults.

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,6 @@ import { defineConfig } from "vite";
 
 export default defineConfig(() => {
   return {
-    plugins: [
-      ViteImageOptimizer({
-        png: {
-          quality: 100
-        },
-        jpeg: {
-          quality: 100
-        },
-        jpg: {
-          quality: 100
-        },
-        webp: {
-          lossless: true
-        }
-      })
-    ]
+    plugins: [ViteImageOptimizer({})]
   };
 });
